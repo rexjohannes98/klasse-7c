1298bf627cfe2fc3d5c971e7614f157f00bc71ef
rexjohannes98 <56408466+rexjohannes98@users.noreply.github.com>
2020-12-17 20:55:02 +0100

    inital

---
+++ app/build/generated/source/buildConfig/debug/space/rexum/klasse/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package space.rexum.klasse;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "space.rexum.klasse";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/build/generated/source/buildConfig/release/space/rexum/klasse/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package space.rexum.klasse;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "space.rexum.klasse";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}

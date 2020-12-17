04da8b713d2b0fa86941d7ee96676bc5e5cd64f2
rexjohannes98 <56408466+rexjohannes98@users.noreply.github.com>
2020-12-17 21:01:38 +0100

    adding first release

---
+++ app/src/main/java/space/rexum/klasse/MainActivity.java
@@ -0,0 +1,17 @@
+package space.rexum.klasse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://klasse-7c.de");
+    }
+}
